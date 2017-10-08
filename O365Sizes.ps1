72d55b8529e272c86fb6f612d30c796504f6da88
DieselTech <dieseltech@protonmail.com>
2017-10-07 20:54:48 -0400

    Create O365Sizes.ps1

---
+++ O365Sizes.ps1
@@ -0,0 +1,5 @@
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+
+get-mailbox | get-mailboxstatistics | ft displayname, totalitemsize 
